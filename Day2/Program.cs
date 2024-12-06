d2b1ad5b7f968a7d3ddfd139c45b4a69c28f3c57
Jon Gordon <jongordn@amazon.com>
2024-12-06 19:32:16 +0000

    Day2-Part2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,4 @@
-// Input
+﻿// Input
 List<List<int>> levels = new List<List<int>>();
 foreach (string line in File.ReadLines($"{Directory.GetCurrentDirectory()}/input.txt"))
 {
@@ -45,3 +45,57 @@ foreach (var level in levels)
 }
 System.Console.WriteLine(safe);
 
+// Part 2
+safe = 0;
+foreach (var level in levels)
+{
+    if (isSafe(level))
+    {
+        ++safe;
+        continue;
+    }
+
+    // Currently unsafe, try remove an index one at a time to see if we find a safe one
+    for (int i = 0; i < level.Count; i++)
+    {
+        List<int> levelCopy = new List<int>(level);
+        levelCopy.RemoveAt(i);
+        if (isSafe(levelCopy))
+        {
+            ++safe;
+            break;
+        }
+    }
+}
+System.Console.WriteLine(safe);
+
+bool isSafe(List<int> level)
+{
+    bool isAscending = true;
+    bool isDescending = true;
+    bool isWithinRange = true;
+
+    for (int i = 1; i < level.Count; i++)
+    {
+        if (level[i] <= level[i - 1])
+            isAscending = false;
+        if (level[i] >= level[i - 1])
+            isDescending = false;
+
+        int delta = Math.Abs(level[i] - level[i - 1]);
+        if (delta < 1 || delta > 3)
+        {
+            isWithinRange = false;
+            break;
+        }
+    }
+
+    if ((!isAscending && !isDescending) || !isWithinRange)
+    {
+        // Unsafe
+        return false;
+    }
+
+    // Can mark as safe
+    return true;
+}
