d118ca494f2d544a1e9621a7816603a2caf4a82a
Jon Gordon <jon@jongor.software>
2024-12-29 19:01:49 +0000

    Day19-Part2

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -40,3 +40,47 @@ bool CanMakeDesign(string design, string[] towels)
 
     return false;
 }
+
+// Part 2
+long totalWays = 0;
+HashSet<string> hashedTowels = new HashSet<string>(input[0].Split(", "));
+var memo = new Dictionary<string, long>();
+
+foreach (var design in designs)
+{
+    totalWays += CountWaysToMakeDesign(design, hashedTowels, memo);
+}
+
+Console.WriteLine(totalWays);
+
+long CountWaysToMakeDesign(string design, HashSet<string> towels, Dictionary<string, long> memo)
+{
+    // Base condition to exit recursion
+    if (string.IsNullOrEmpty(design))
+    {
+        return 1;
+    }
+
+    // Check if the result is already computed for the current substring
+    if (memo.ContainsKey(design))
+    {
+        return memo[design];
+    }
+
+    long count = 0;
+    foreach (var towel in towels)
+    {
+        if (design.StartsWith(towel))
+        {
+            count += CountWaysToMakeDesign(design.Substring(towel.Length), towels, memo);
+        }
+    }
+
+    // Store the result in the memo dictionary for the current substring
+    memo[design] = count;
+
+    // Debugging output
+    Console.WriteLine($"Memoizing: {design} -> {count}");
+
+    return count;
+}
